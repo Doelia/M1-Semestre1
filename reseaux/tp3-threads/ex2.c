684ba670067970bda5c8aec142ca9218d19a3fc4
Doelia <kyxalia@gmail.com>
2014-11-25 14:15:01 +0100

    modification des noms de repertoires

---
+++ reseaux/tp3-threads/ex2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+
+void* guichet() {
+
+}
+
+int main() {
+	
+
+}
+
---
+++ reseaux/tp4-chat/main.cc
@@ -0,0 +1,9 @@
+#include "sockdist.h"
+#include "sock.h"
+
+using namespace std;
+
+int main() {
+	cout << "Hello" << endl;
+}
+

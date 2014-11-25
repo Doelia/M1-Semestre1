89a86449c625efec4ca545c9a5f402032e216863
Doelia <kyxalia@gmail.com>
2014-11-25 14:13:59 +0100

    Ajout début de client/server

--- reseaux/tp4/main.cc
+++
@@ -1,9 +0,0 @@
-#include "sockdist.h"
-#include "sock.h"
-
-using namespace std;
-
-int main() {
-	cout << "Hello" << endl;
-}
-
--- reseaux/tp5/test.x
+++
@@ -1,5 +0,0 @@
-program TEST { 
-	version TEST_VERSION_1 { 
-		void HELLO(arg0) = 1; 
-	} = 1; 
-} = 0x20000001;

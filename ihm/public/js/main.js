b73b851a7f83a5ae0e25746eeefff615608ab353
Doelia <kyxalia@gmail.com>
2014-11-26 19:33:11 +0100

    main.js

--- ihm/public/js/main.js
+++ ihm/public/js/main.js
@@ -8,13 +8,62 @@ function stopAll(msg) {
 	$('body').html(msg);
 }
 
+function CanvasObject() {
+	this.color = "red";
+	this.canvas = $("#canvas");
+	this.context = this.canvas[0].getContext('2d');
+	this.width_brush = 3;
+	this.draw = false;
+	this.last = null;
+	var that = this;
+
+	this.drawLine = function(a, b) {
+		this.context.beginPath();
+		this.context.moveTo(a.x, a.y);
+		this.context.lineTo(b.x, b.y);
+		this.context.strokeStyle = this.color;
+		this.context.lineWidth = this.width_brush;
+		this.context.stroke();
+	}
+
+	this.getPositionCursor = function(e) {
+		 var parentOffset = that.canvas.parent().offset(); 
+   		var relX = e.pageX - parentOffset.left;
+   		var relY = e.pageY - parentOffset.top;
+   		return {x: relX, y: relY};
+	}
+
+	this.canvas.mousedown(function(e) {
+		that.last = that.getPositionCursor(e);
+		that.draw = true;
+	});
+
+	this.canvas.mouseup(function() {
+		that.draw = false;
+	});
+
+	this.canvas.mousemove(function(e) {
+		if (that.draw) {
+			var pos = that.getPositionCursor(e);
+			socket.emit('drawLine', that.last, pos);
+			that.drawLine(that.last, pos);
+			that.last = pos;
+		}
+	});
+}
+
+var canvasObj;
 
 function start_loading(login) {
 	
-	socket = io.connect('http://pc.doelia.fr:8080');
-	socket.on('connexion_ok', function() {
+	socket = io.connect('http://localhost:8080');
 
+	socket.on('drawLine', function(a, b) {
+		console.log(a);
+		canvasObj.drawLine(a,b);
 	});
+
+
 	socket.on('close', function() {
 		stopAll();
 	});
@@ -26,45 +75,9 @@ function start_loading(login) {
 
 $(document).ready(function() {
 
-	var color = "green";
-	var canvas = $("#canvas");
-	var context = canvas[0].getContext('2d');
-	var painting = false;
-	var started = false;
-	var width_brush = 1;
-
-	function drawLine() {
-		if (!started) {
-			context.beginPath();
-			context.moveTo(cursorX, cursorY);
-			started = true;
-		} 
-		else {
-			context.lineTo(cursorX, cursorY);
-			context.strokeStyle = color;
-			context.lineWidth = width_brush;
-			context.stroke();
-		}
-	}
-
-	canvas.mousedown(function(e) {
-		painting = true;
-		cursorX = (e.pageX - this.offsetLeft);
-		cursorY = (e.pageY - this.offsetTop);
-	});
-
-	$(this).mouseup(function() {
-		painting = false;
-		started = false;
-	});
-
-	canvas.mousemove(function(e) {
-		if (painting) {
-			cursorX = (e.pageX - this.offsetLeft) - 10; 
-			cursorY = (e.pageY - this.offsetTop) - 10;
-			drawLine();
-		}
-	});
+start_loading('doelia');
+canvasObj = new CanvasObject();
+	
 
 });
 
